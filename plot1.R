d7e5e7264883daaa65402d56c5d352c6c78a1c15
Bruce <git@gayleard.net>
2017-09-09 13:28:53 +1000

    fix a few comments

--- plot1.R
+++ plot1.R
@@ -1,10 +1,10 @@
-# read the data file which is in a sub-directory of the wroking directory
+# read the data file from the plot_data sub-directory of the working directory
 allData <- read.table("./plot_data/household_power_consumption.txt", header = TRUE, sep = ";", na.strings = "?")
 
-# grab the data for the 2 dates that will be plotted
+# grab the data for the 2 days to be plotted
 plotData <- allData[(allData$Date == "1/2/2007" | allData$Date == "2/2/2007"),]
 
-# set up the output png file
+# set up the output png file in the plot_data sub-directory of the working directory
 png(filename = "./plot_data/plot1.png", width = 480, height = 480)
 
 # create the plot
--- plot4.R
+++ plot4.R
@@ -1,13 +1,13 @@
-# read the data file which is in a sub-directory of the wroking directory
+# read the data file from the plot_data sub-directory of the working directory
 allData <- read.table("./plot_data/household_power_consumption.txt", header = TRUE, sep = ";", na.strings = "?")
 
-# grab the data for the 2 dates that will be plotted
+# grab the data for the 2 days to be plotted
 plotData <- allData[(allData$Date == "1/2/2007" | allData$Date == "2/2/2007"),]
 
 # combine Data and Time into a new DataTime column
 plotData$DateTime <- strptime(paste(plotData$Date, plotData$Time, sep = " "), format = "%d/%m/%Y %H:%M:%S")
 
-# set up the output png file
+# set up the output png file in the plot_data sub-directory of the working directory
 png(filename = "./plot_data/plot4.png", width = 480, height = 480)
 
 # 4 plots 2 x 2
